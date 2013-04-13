61555d3a197a22af7f601513c5ddcef6b5a8a8ef
Daiz <taneli.vatanen@gmail.com>
2013-04-13 16:15:21 +0300

    Initial commit with CLI outline

---
+++ crank.js
@@ -0,0 +1,33 @@
+/*jshint node:true */
+
+var program = require('commander');
+
+// > crank init [plugin/behavior/effect]
+// Initializes an addon project with the correct template files.
+program
+  .command('init [type]')
+  .description("Initialize an addon. Possible types: plugin, behavior, effect")
+  .action(function(type) {
+
+  });
+
+// > crank build
+// Compiles the current plugin into a .c2addon file.
+program
+  .command('build')
+  .description('Build the current project into a .c2addon file.')
+  .action(function() {
+
+  });
+
+// > crank watch
+// Watches project files for any changes and copies them to %AppData%\Construct2\plugins\<pluginid>
+// for quick previewing. Only available on Windows.
+program
+  .command('watch')
+  .description('Watch project files for changes and update the addon in Construc 2.')
+  .action(function() {
+
+  });
+
+program.parse(process.argv);
