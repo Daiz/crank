b73e176b9ac929d9fb1bccdc60e2700c7bfabdd7
Daiz <taneli.vatanen@gmail.com>
2013-04-14 19:10:16 +0300

    Add build task

--- tasks/build.js
+++ tasks/build.js
@@ -1,13 +1,14 @@
 /*jshint node:true */
 
 var builder = require('xmlbuilder'),
-    AdmZip  = require('adm-zip');
-
+    fs      = require('fs'),
+    wrench  = require('wrench'),
+    zip     = new require('node-zip')();
 
 module.exports = function(addon) {
-  
+
   // build the xml from addon.json
-  var xml = builder.create("c2addon", {encoding: "UTF-8"});
+  var xml = builder.create("c2addon", {version: "1.0", encoding: "UTF-8"});
   xml.com("One of: plugin, behavior, effect");
   xml.ele("type", addon.type);
   xml.ele("name", addon.name);
@@ -17,7 +18,27 @@ module.exports = function(addon) {
   xml.ele("documentation", addon.documentation);
   xml.ele("description", addon.description);
 
-  var type = addon.type;
+  // read the contents of our src folder
+  var filepaths = wrench.readdirSyncRecursive('./src');
+  var filepath, write;
 
+  // zip it up
+  zip.file('info.xml', xml.end({pretty: true}));
+  var folder = zip.folder('files' + (addon.type === "effect" ? '' : '/' + addon.id));
+  for(var i = 0, ii = filepaths.length; i < ii; i++) {
+    write = true;
+    filepath = filepaths[i].replace(/\\/g,'/');
+    if(!filepath.match(/\./)) {
+      if(fs.lstatSync('./src/' + filepath).isDirectory()) {
+        write = false;
+      }
+    }
+    if(write) {
+      folder.file(filepath, fs.readFileSync('./src/' + filepath, 'utf8'));
+    }
+  }
+  folder.file('PluginIcon.ico', fs.readFileSync('./PluginIcon.ico', 'base64'), {base64: true});
+  var data = zip.generate({base64: true, compression: 'STORE'});
+  fs.writeFileSync(addon.id + '.c2addon', data, 'base64');
 
 };
