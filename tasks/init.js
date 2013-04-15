ef278ebfb843b41d6b348f8d4ef6859b2f251d40
Daiz <taneli.vatanen@gmail.com>
2013-04-15 18:56:08 +0300

    Update template paths in init task

--- tasks/init.js
+++ tasks/init.js
@@ -7,15 +7,15 @@ module.exports = function(type) {
 
   switch(type) {
     case "plugin":
-      wrench.copyDirSyncRecursive(path.resolve(__dirname, "../templates/plugin"), cwd);
+      wrench.copyDirSyncRecursive(path.resolve(__dirname, "../js/templates/plugin"), cwd);
       console.log("Initialized a new plugin project.");
       break;
     case "behavior":
-      wrench.copyDirSyncRecursive(path.resolve(__dirname, "../templates/behavior"), cwd);
+      wrench.copyDirSyncRecursive(path.resolve(__dirname, "../js/templates/behavior"), cwd);
       console.log("Initialized a new behavior project.");
       break;
     case "effect":
-      wrench.copyDirSyncRecursive(path.resolve(__dirname, "../templates/effect"), cwd);
+      wrench.copyDirSyncRecursive(path.resolve(__dirname, "../js/templates/effect"), cwd);
       console.log("Initialized a new effect project.");
       break;
     default:
